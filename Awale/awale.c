b22567c303117c1143f09c91a1447509537e9a2b
DiegoscomV3 <opgems9000@gmail.com>
2024-11-07 20:16:46 +0100

    Finition Fonctions Jeu p1
    Co-authored-by: hary42 <hary42@users.noreply.github.com>

--- Awale/awale.c
+++ Awale/awale.c
@@ -9,7 +9,7 @@ Plateau* init(){
     p->grainesJ1 = p->grainesJ2 = 0;
 
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//SENS HORAIRE A DEFINIR par joueurs////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     p->sensJeu = HORAIRE;
 
 
@@ -22,75 +22,112 @@ void end(Plateau* p){
     free(p);
 }
 
-bool play(Plateau* p, unsigned short int num_case){
-    if (p->JoueurCourant == JOUEUR1 && (num_case < 6 || num_case > 11))
+void changePlayer(Plateau* p){
+    switch(p->JoueurCourant){
+        case JOUEUR1:
+            p->JoueurCourant = JOUEUR2;
+            break;
+        case JOUEUR2:
+            p->JoueurCourant = JOUEUR1;
+            break;
+    }
+}
+
+Bool canPlay(Plateau* p, unsigned char num_case){
+	  if (p->JoueurCourant == JOUEUR1 && (num_case < 6 || num_case > 11))
         return false;
     if (p->JoueurCourant == JOUEUR2 && num_case > 5)
         return false;
     if (p->cases[num_case] == 0)
         return false;
-    unsigned short int originalNum_case = num_case;
+    return true;
+}
+
+void gererDepassement(unsigned char* num_case){
+    if (num_case == 12) *num_case = 0; 
+    else if (num_case > 12) *num_case = 11; // unsigned char => (0 - 1) == (255)
+}
+
+BitField play(Plateau* p, unsigned char num_case){
+    unsigned char originalNum_case = num_case;
+  	
     int nbSeeds = p->cases[num_case];
     p->cases[num_case] = 0;
     do {
         num_case += p->sensJeu;
-        if (num_case == 12)
-            num_case = 0;
-        else if (num_case > 12)
-            num_case = 11;
+        gererDepassement(&num_case);
         if (num_case != originalNum_case) {
-            ++p->cases[num_case];
+            ++p->cases[num_case]
             --nbSeeds;
         }
     }while (nbSeeds);
-    return true;
+    return gererConquetes(p,num_case);
 }
 
+            
+BitField gererConquetes(Plateau* p, unsigned char num_case){
+    BitField caseConquise = 0;
+    while (p->cases[num_case] == 2 || p->cases[num_case] == 3 && ((p->JoueurCourant == JOUEUR1 && num_case > 5) || (p->JoueurCourant == JOUEUR2 && num_case < 6))){
+        caseConquise |= (1 << num_case);
+
+        if (p->JoueurCourant == JOUEUR1)  
+            p->grainesJ1 += p->cases[num_case];
+        else //JOUEUR2
+            p->grainesJ2 += p->cases[num_case]; 
+        p->cases[num_case] = 0;
+        
+        num_case -= p->sensJeu;
+        gererDepassement(&num_case);
+    }
+    return caseConquise;
+}
+
+
 void printBoard(Plateau* p, char* buffer){
-    switch(p->JoueurCourant){
-        case JOUEUR1:
-            if (p->sensJeu == HORAIRE)
-                sprintf(buffer,
-                    "  ╔══╦══╦══╦══╦══╦══╗\n"
-                    "> ║%02d║%02d║%02d║%02d║%02d║%02d║ ┐\n"
-                    "│ ╠══╬══╬══╬══╬══╬══╣ │\n"
-                    "└ ║%02d║%02d║%02d║%02d║%02d║%02d║ <\n"
-                    "  ╚══╩══╩══╩══╩══╩══╝\n",
-                    p->cases[0], p->cases[1], p->cases[2], p->cases[3], p->cases[4], p->cases[5],
-                    p->cases[11], p->cases[10], p->cases[9], p->cases[8], p->cases[7], p->cases[6]
-                );
-            else
-                sprintf(buffer,
-                    "  ╔══╦══╦══╦══╦══╦══╗\n"
-                    "┌ ║%02d║%02d║%02d║%02d║%02d║%02d║ <\n"
-                    "│ ╠══╬══╬══╬══╬══╬══╣ │\n"
-                    " >║%02d║%02d║%02d║%02d║%02d║%02d║ ┘\n"
-                    "  ╚══╩══╩══╩══╩══╩══╝\n",
-                    p->cases[0], p->cases[1], p->cases[2], p->cases[3], p->cases[4], p->cases[5],
-                    p->cases[11], p->cases[10], p->cases[9], p->cases[8], p->cases[7], p->cases[6]
-                );
+    switch((int)p->JoueurCourant * (int)p->sensJeu){
+        case (int)JOUEUR1* (int)HORAIRE:
+            sprintf(buffer,
+                "  ╔══╦══╦══╦══╦══╦══╗\n"
+                "> ║%02d║%02d║%02d║%02d║%02d║%02d║ ┐\n"
+                "│ ╠══╬══╬══╬══╬══╬══╣ │\n"
+                "└ ║%02d║%02d║%02d║%02d║%02d║%02d║ <\n"
+                "  ╚══╩══╩══╩══╩══╩══╝\n",
+                p->cases[0], p->cases[1], p->cases[2], p->cases[3], p->cases[4], p->cases[5],
+                p->cases[11], p->cases[10], p->cases[9], p->cases[8], p->cases[7], p->cases[6]
+            );
             break;
-        case JOUEUR2:
-            if (p->sensJeu == HORAIRE)
-                sprintf(buffer,
-                    "  ╔══╦══╦══╦══╦══╦══╗\n"
-                    "> ║%02d║%02d║%02d║%02d║%02d║%02d║ ┐\n"
-                    "│ ╠══╬══╬══╬══╬══╬══╣ │\n"
-                    "└ ║%02d║%02d║%02d║%02d║%02d║%02d║ <\n"
-                    "  ╚══╩══╩══╩══╩══╩══╝\n",
-                p->cases[6], p->cases[7], p->cases[8], p->cases[9], p->cases[10], p->cases[11],
-                p->cases[5], p->cases[4], p->cases[3], p->cases[2], p->cases[1], p->cases[0]
-                );
-            else
-                sprintf(buffer,
-                    "  ╔══╦══╦══╦══╦══╦══╗\n"
-                    "┌ ║%02d║%02d║%02d║%02d║%02d║%02d║ <\n"
-                    "│ ╠══╬══╬══╬══╬══╬══╣ │\n"
-                    " >║%02d║%02d║%02d║%02d║%02d║%02d║ ┘\n"
-                    "  ╚══╩══╩══╩══╩══╩══╝\n",
-                p->cases[6], p->cases[7], p->cases[8], p->cases[9], p->cases[10], p->cases[11],
-                p->cases[5], p->cases[4], p->cases[3], p->cases[2], p->cases[1], p->cases[0]
-                );
+        case (int)JOUEUR1* (int)AHORAIRE:
+            sprintf(buffer,
+                "  ╔══╦══╦══╦══╦══╦══╗\n"
+                "┌ ║%02d║%02d║%02d║%02d║%02d║%02d║ <\n"
+                "│ ╠══╬══╬══╬══╬══╬══╣ │\n"
+                " >║%02d║%02d║%02d║%02d║%02d║%02d║ ┘\n"
+                "  ╚══╩══╩══╩══╩══╩══╝\n",
+                p->cases[0], p->cases[1], p->cases[2], p->cases[3], p->cases[4], p->cases[5],
+                p->cases[11], p->cases[10], p->cases[9], p->cases[8], p->cases[7], p->cases[6]
+            );
+            break;
+        case (int)JOUEUR2 * (int)HORAIRE:
+            sprintf(buffer,
+                "  ╔══╦══╦══╦══╦══╦══╗\n"
+                "> ║%02d║%02d║%02d║%02d║%02d║%02d║ ┐\n"
+                "│ ╠══╬══╬══╬══╬══╬══╣ │\n"
+                "└ ║%02d║%02d║%02d║%02d║%02d║%02d║ <\n"
+                "  ╚══╩══╩══╩══╩══╩══╝\n",
+            p->cases[6], p->cases[7], p->cases[8], p->cases[9], p->cases[10], p->cases[11],
+            p->cases[5], p->cases[4], p->cases[3], p->cases[2], p->cases[1], p->cases[0]
+            );
+            break;
+        case (int)JOUEUR2 * (int)AHORAIRE:
+            sprintf(buffer,
+                "  ╔══╦══╦══╦══╦══╦══╗\n"
+                "┌ ║%02d║%02d║%02d║%02d║%02d║%02d║ <\n"
+                "│ ╠══╬══╬══╬══╬══╬══╣ │\n"
+                " >║%02d║%02d║%02d║%02d║%02d║%02d║ ┘\n"
+                "  ╚══╩══╩══╩══╩══╩══╝\n",
+            p->cases[6], p->cases[7], p->cases[8], p->cases[9], p->cases[10], p->cases[11],
+            p->cases[5], p->cases[4], p->cases[3], p->cases[2], p->cases[1], p->cases[0]
+            );
             break;
         default:
             perror("joueur non défini");
@@ -98,112 +135,54 @@ void printBoard(Plateau* p, char* buffer){
     }
 }
 
-bool hasWon(Plateau* p){
-    switch(p->JoueurCourant){
-        case JOUEUR1:
-            if (p->grainesJ1 > 24)
-                return true;
-            return false;
-        case JOUEUR2:
-            if (p->grainesJ2 > 24)
-                return true;
-            return false;
-    }
+Bool hasWon(Plateau* p){
+    if(p->JoueurCourant == JOUEUR1  && p->grainesJ1 > 24) return true;
+    if(p->JoueurCourant == JOUEUR2  && p->grainesJ2 > 24) return true;
+    return false;
 }
 
-bool isDraw(Plateau* p){
+Bool isDraw(Plateau* p){
     if (p->grainesJ1 == 24 && p->grainesJ2 == 24)
         return true;
     return false;
 }
 
-casesPermises playableFamine(Plateau* p){
-    casesPermises casesAutorise = {0, 0, 0, 0, 0, 0};
-    int startInd;
-    switch(p->JoueurCourant){
-        case JOUEUR1:
-            if (p->sensJeu == HORAIRE)
-                startInd = 6;
-            else
-                startInd = 11;
-            break;
-        case JOUEUR2:
-            if (p->sensJeu == HORAIRE)
-                startInd = 0;
-            else
-                startInd = 5;
-            break;
-    }
-    if (p->cases[startInd] >= 6) {
-        if (p->sensJeu == HORAIRE)
-            casesAutorise->case6 = 1;
-        else
-            casesAutorise->case1 = 1;
-    }
-    if (p->cases[startInd + p->sensJeu] >= 5) {
-        if (p->sensJeu == HORAIRE)
-            casesAutorise->case5 = 1;
-        else
-            casesAutorise->case2 = 1;
-    }
-    if (p->cases[startInd + 2 * p->sensJeu] >= 4) {
-        if (p->sensJeu == HORAIRE)
-            casesAutorise->case4 = 1;
-        else
-            casesAutorise->case3 = 1;
-    }
-    if (p->cases[startInd + 3 * p->sensJeu] >= 3) {
-        if (p->sensJeu == HORAIRE)
-            casesAutorise->case3 = 1;
-        else
-            casesAutorise->case4 = 1;
-    }
-    if (p->cases[startInd + 4 * p->sensJeu] >= 2) {
-        if (p->sensJeu == HORAIRE)
-            casesAutorise->case2 = 1;
-        else
-            casesAutorise->case5 = 1;
-    }
-    if (p->cases[startInd + 5 * p->sensJeu] >= 1) {
-        if (p->sensJeu == HORAIRE)
-            casesAutorise->case1 = 1;
-        else
-            casesAutorise->case6 = 1;
+BitField playableFamine(Plateau* p){
+    BitField casesAutorise = 0; // les bits de 1 à 6 indiquent si les cases 1-6(Joueur1) ou 7-12 (Joueur2) sont jouables (égal à 1).
+    int ajout = 1;
+    int offset = p->JoueurCourant == JOUEUR2 ? 6 : 0;;
+
+    for(int i = offset; i < 6 + offset; i++) {
+        /*
+        Cas Horaire  : p->cases[i] >= 6 - (i - offset)
+        Cas Ahoraire : p->cases[i] >= 1 + (i - offset)
+        */
+        if(p->cases[i] >= (((int)(p->sensJeu) == HORAIRE ? 6:1) - (int)(p->sensJeu) * (i - offset))){
+            casesAutorise |= ajout; 
+            ajout <<= 1;
+        }
     }
     return casesAutorise;
 }
 
 void collectAllPoints(Plateau* p){
-    switch(p->JoueurCourant){
-        case JOUEUR1:
-            for (int i = 6 ; i < 12; ++i){
-                p->grainesJ1 += p->cases[i];
-                p->cases[i] = 0;
-            }
-            break;
-        case JOUEUR2:
-            for (int i = 0 ; i < 6; ++i){
-                p->grainesJ2 += p->cases[i];
-                p->cases[i] = 0;
-            }
-            break;
+    if(p->JoueurCourant == JOUEUR1)
+        for (int i = 6 ; i < 12; ++i){
+            p->grainesJ1 += p->cases[i];
+            p->cases[i] = 0;
+        }
+    else{ //JOUEUR2
+        for (int i = 0 ; i < 6; ++i){
+            p->grainesJ2 += p->cases[i];
+            p->cases[i] = 0;
+        }
     }
 }
 
 Bool isOpponentFamished(Plateau* p){
-    switch(p->JoueurCourant){
-        case JOUEUR1:
-            for (int i = 0 ; i < 6; ++i){
-                if (p->cases[i] != 0)
-                    return false;
-            }
-            break;
-        case JOUEUR2:
-            for (int i = 6 ; i < 12; ++i){
-                if (p->cases[i] != 0)
-                    return false;
-            }
-            break;
-    }
+    int i;
+    int offset = p->JoueurCourant == JOUEUR2 ? 6 : 0;
+    for (i = offset ; i < 6 + offset; ++i)
+        if(p->cases[i] != 0) return false;
     return true;
 }
--- Awale/awale.h
+++ Awale/awale.h
@@ -5,22 +5,15 @@
 #define false 0
 #define true 1
 
-typedef unsigned short int Graines;
-typedef unsigned short int Bool;
+typedef unsigned char Graines;
+typedef unsigned char Bool;
+typedef unsigned short int BitField; //Bitfield pour indiquer si une case est validée (1) ou pas (0)
 typedef enum {JOUEUR1 = 1, JOUEUR2 = 2} Joueur;
 typedef enum {AHORAIRE = -1, HORAIRE = 1} Sens;
 
-typedef struct bitField{
-    Bool case1 : 1; // store 1 bit
-    Bool case2 : 1;
-    Bool case3 : 1;
-    Bool case4 : 1;
-    Bool case5 : 1;
-    Bool case6 : 1;
-} casesPermises;
 
 typedef struct plateau{
-    Graines cases[NB_CASES]; // plateau avec ranges[i] le nombre de graines dans la case i+1. Le joueur 1 a les cases 1-6 et le joueur 2 7-12
+    Graines cases[NB_CASES]; // plateau avec ranges[i] le nombre de graines dans la case i+1. Le joueur 1 a les cases 7-12 et le joueur 2 1-6
     Graines grainesJ1; // graines du joueur 1
     Graines grainesJ2; // graines du joueur 2
     Joueur JoueurCourant;
@@ -29,8 +22,10 @@ typedef struct plateau{
 
 Plateau* init();
 void end(Plateau* p);
-Bool play(Plateau* p, unsigned short int num_case);
-casesPermises playableFamine(Plateau* p);
+BitField play(Plateau* p, unsigned char num_case);
+Bool canPlay(Plateau* p, unsigned char num_case);
+void gererDepassement(unsigned char* num_case);
+BitField playableFamine(Plateau* p);
 void printBoard(Plateau* p, char* buffer);
 Bool hasWon(Plateau* p);
 Bool isDraw(Plateau* p);
